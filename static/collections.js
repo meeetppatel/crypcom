e18f5597d0d83760007e99cdba74f67246a8f942
Meet Patel <ptlmeet2783@gmail.com>
2023-05-23 18:02:12 +0530

    add

---
+++ static/collections.js
@@ -0,0 +1,18 @@
+import banner from '../assets/BoredApeBanner.png'
+import logo from '../assets/Bored-Ape-Yacht-Club.png'
+
+export const collectionData = {
+  name: 'Bored Ape Yacht Club',
+  description:
+    'Lorem ipsum dolor sit amet. A vitae consectetur qui veritatis ullam vel autem quas ea quam temporibus. Ut molestiae eveniet et nulla maiores qui quia aliquid est debitis laudantium est laboriosam tenetur.',
+  isVerified: true,
+  banner_image_url: banner,
+  image_url: logo,
+  stats: {
+    total_volume: 907100,
+    total_supply: 10000,
+    num_owners: 3500,
+    floor_price: 22.6,
+  },
+  address: '0x3fd3cA343B65A7AC983744A29FC1dC8811c008E7',
+}
